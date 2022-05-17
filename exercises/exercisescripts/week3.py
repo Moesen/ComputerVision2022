5dd9bd5cde0a235b9cd1751aed876556c648ce2a
Moesen <gustavmoesmand@gmail.com>
2022-05-17 15:29:26 +0200

    added week3 first part

--- exercises/exercisescripts/week3.py
+++ exercises/exercisescripts/week3.py
@@ -5,18 +5,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from pathlib import Path
+from scipy.spatial.transform import Rotation
 
 S = ImgSaver()
+k = np.array([[1000, 0, 300], [0, 1000, 200], [0, 0, 1]])
 
+r1 = np.identity(3)
+t1 = np.array([0, 0, 0]).reshape((-1, 1))
+
+r2 = Rotation.from_euler("xyz", [0.7, -0.5, 0.8]).as_matrix()
+t2 = np.array([0.2, 2, 1]).reshape((-1, 1))
 
 def test_ex1():
     q1, q2 = ex1()
-
+    s = f"{q1=}\n {q2=}"
+    S.save_txt("ex3-1", s)
 
 def ex1():
+    Q = np.array([1, .5, 4]).reshape(-1, 1)
+    q1, q2 = u.projectpoints(k, r1, t1, Q), u.projectpoints(k, r2, t2, Q)
 
     return q1, q2
 
 
 if __name__ == "__main__":
-    pass
+    test_ex1()
